d400bc6ba99325ab6c41a9e87c8dbb60762902ac
zhong huang <zhonghuang240@gmail.com>
2022-05-02 10:40:21 -0400

    tested on arm

--- potato.py
+++ potato.py
@@ -1,6 +1,5 @@
 import time
 import serial
-import numpy as np
 import geometry_msgs.msg
 import sys
 import copy
@@ -17,7 +16,10 @@ from moveit_commander.conversions import pose_to_list
 #from robotiq_2f_gripper_control.msg import _Robotiq2FGripper_robot_output  as outputMsg
 from time import sleep
 
-from move_group_python_interface_tutorial_wo_gripper_wo_custom_topic import all_close, MoveGroupPythonIntefaceTutorial
+from move_group_python_interface_tutorial_w_gripper_wo_custom_topic import all_close, MoveGroupPythonIntefaceTutorial
+
+
+import numpy as np
 
 class Arm(object):
     """Robot arm"""
@@ -60,7 +62,7 @@ class Arm(object):
         self.RZ_Y_AXIS = np.pi * (3/4) # across table
 
         # home position - gripper up and out of view of camera
-        self.HOME_POSITION = [0.75, 0.0, 0.35, 0.0, 0.7071, 0.0, 0.7071]
+        self.HOME_POSITION = [200, -600, 300] + self.get_quaternion_from_euler(0,0,0)
 
         # command coordinates
         self.x = 0.75
@@ -80,8 +82,10 @@ class Arm(object):
         # self.arduino = serial.Serial('COM3', 115200, timeout=1)
         # self.arduino.flushInput()
         # self.arduino.flushOutput()
+        # time.sleep(1)
         self.grip_state = False
-        self.force_reading = 0 # load cell reading
+        self.force_reading_1 = 0 # load cell reading
+        self.force_reading_2 = 0
 
         # number of bricks stacked (1 - 16)
         self.stack_number = stack_number
@@ -98,17 +102,16 @@ class Arm(object):
         print(self.state)
 
     def read_arduino(self):
-    # read grip state
-    # read force error
-    # read collision error
-        pass
-
-    def get_quaternion_from_euler(self, roll, pitch, yaw):
-        qx = np.sin(roll / 2) * np.cos(pitch / 2) * np.cos(yaw / 2) - np.cos(roll / 2) * np.sin(pitch / 2) * np.sin(yaw / 2)
-        qy = np.cos(roll / 2) * np.sin(pitch / 2) * np.cos(yaw / 2) + np.sin(roll / 2) * np.cos(pitch / 2) * np.sin(yaw / 2)
-        qz = np.cos(roll / 2) * np.cos(pitch / 2) * np.sin(yaw / 2) - np.sin(roll / 2) * np.sin(pitch / 2) * np.cos(yaw / 2)
-        qw = np.cos(roll / 2) * np.cos(pitch / 2) * np.cos(yaw / 2) + np.sin(roll / 2) * np.sin(pitch / 2) * np.sin(yaw / 2)
-        return [qx, qy, qz, qw]
+        data = self. arduino.readline()
+        try:
+            data = data.decode()
+            data = data.strip()
+            data = data.split()
+            if len(data) == 3:
+                data = list(map(float, data))
+                return data
+        except:
+            return None
 
     def next_stack_position(self):
         if self.stack_number == 1 or self.stack_number == 9:
@@ -203,22 +206,39 @@ class Arm(object):
         # returns position of next stack
         return [x, y, z, Rx, Ry, Rz]
 
+    def get_quaternion_from_euler(self, roll_in, pitch_in, yaw_in):
+        roll = roll_in
+        pitch = pitch_in + np.pi * 0.5
+        yaw = yaw_in + np.pi * 0.25
+        qx = np.sin(roll / 2) * np.cos(pitch / 2) * np.cos(yaw / 2) - np.cos(roll / 2) * np.sin(pitch / 2) * np.sin(yaw / 2)
+        qy = np.cos(roll / 2) * np.sin(pitch / 2) * np.cos(yaw / 2) + np.sin(roll / 2) * np.cos(pitch / 2) * np.sin(yaw / 2)
+        qz = np.cos(roll / 2) * np.cos(pitch / 2) * np.sin(yaw / 2) - np.sin(roll / 2) * np.sin(pitch / 2) * np.cos(yaw / 2)
+        qw = np.cos(roll / 2) * np.cos(pitch / 2) * np.cos(yaw / 2) + np.sin(roll / 2) * np.sin(pitch / 2) * np.sin(yaw / 2)
+        return [qx, qy, qz, qw]
+
     def pose_from_list(self, pose_list):
         wpose = geometry_msgs.msg.Pose()
-        wpose.position.x = pose_list[0] / 1000 # convert mm to m
-        wpose.position.y = pose_list[1] / 1000
-        wpose.position.z = pose_list[2] / 1000
         wpose.orientation.x = pose_list[3]
         wpose.orientation.y = pose_list[4]
         wpose.orientation.z = pose_list[5]
         wpose.orientation.w = pose_list[6]
+        wpose.position.x = pose_list[0] * 0.001
+        wpose.position.y = pose_list[1] * 0.001 
+        wpose.position.z = pose_list[2] * 0.001
 
         return wpose
 
     def send_arm_pose_cmd(self, pose_list):
         # sends position target to arm
         pose = self.pose_from_list(pose_list)
-        self.UR5_object.go_to_pose_goal(pose)
+        print('planning path')
+        print(pose_list)
+        print(pose)
+        waypoints = []
+        waypoints.append(copy.deepcopy(pose))
+        cartesian_plan, fraction = self.UR5_object.plan_cartesian_path(waypoints=waypoints)
+        raw_input()
+        self.UR5_object.execute_plan(cartesian_plan)
 
     ### state machine ###
     def main(self):
@@ -237,12 +257,58 @@ class Arm(object):
                 # initializations, calibrations etc
                 if self.state_first_run:
                     # move gripper to home position
+                    print('home position')
+                    raw_input()
                     self.send_arm_pose_cmd(self.HOME_POSITION)
+                    self.x = self.HOME_POSITION[0]
+                    self.y = self.HOME_POSITION[1]
+                    self.z = self.HOME_POSITION[2]
+                    self.Rx = 0
+                    self.Ry = 0
+                    self.Rz = 0
                     self.state_first_run = False
+
+                self.x += 300
+                self.y += 1100
+                new_list = [self.x, self.y, self.z] + self.get_quaternion_from_euler(self.Rx, self.Ry, self.Rz)
+                raw_input()
+                self.send_arm_pose_cmd(new_list)
+
+                self.z -= 200
+                new_list = [self.x, self.y, self.z] + self.get_quaternion_from_euler(self.Rx, self.Ry, self.Rz)
+                raw_input()
+                self.send_arm_pose_cmd(new_list)
+
+                self.z += 200
+                new_list = [self.x, self.y, self.z] + self.get_quaternion_from_euler(self.Rx, self.Ry, self.Rz)
+                raw_input()
+                self.send_arm_pose_cmd(new_list)
+
+                self.y -= 1100
+                new_list = [self.x, self.y, self.z] + self.get_quaternion_from_euler(self.Rx, self.Ry, self.Rz)
+                raw_input()
+                self.send_arm_pose_cmd(new_list)
+
+                self.z -= 200
+                new_list = [self.x, self.y, self.z] + self.get_quaternion_from_euler(self.Rx, self.Ry, self.Rz)
+                raw_input()
+                self.send_arm_pose_cmd(new_list)
+
+                self.z -= 50
+                new_list = [self.x, self.y, self.z] + self.get_quaternion_from_euler(self.Rx, self.Ry, self.Rz)
+                raw_input()
+                self.send_arm_pose_cmd(new_list)
+
+                self.z += 250
+                new_list = [self.x, self.y, self.z] + self.get_quaternion_from_euler(self.Rx, self.Ry, self.Rz)
+                raw_input()
+                self.send_arm_pose_cmd(new_list)
+
 
                 if time.time() >= self.state_start_time + 10:
                     #self.state_step('EMPTY_DWELL')
                     pass
+
 
             elif self.state == "EMPTY_DWELL":
                 # gripper at home position with no brick
@@ -501,7 +567,11 @@ def test_pose_from_list(pose_list):
 
     return wpose
 
-def test_get_quaternion_from_euler(roll, pitch, yaw):
+def test_get_quaternion_from_euler(roll_in, pitch_in, yaw_in):
+    roll = roll_in
+    pitch = pitch_in + np.pi
+    yaw = yaw_in
+    print(roll, pitch, yaw)
     qx = np.sin(roll / 2) * np.cos(pitch / 2) * np.cos(yaw / 2) - np.cos(roll / 2) * np.sin(pitch / 2) * np.sin(yaw / 2)
     qy = np.cos(roll / 2) * np.sin(pitch / 2) * np.cos(yaw / 2) + np.sin(roll / 2) * np.cos(pitch / 2) * np.sin(yaw / 2)
     qz = np.cos(roll / 2) * np.cos(pitch / 2) * np.sin(yaw / 2) - np.sin(roll / 2) * np.sin(pitch / 2) * np.cos(yaw / 2)
@@ -512,33 +582,35 @@ def test_get_quaternion_from_euler(roll, pitch, yaw):
 if __name__ == "__main__":
 
     UR5 = MoveGroupPythonIntefaceTutorial()
+    arm = Arm(COM_port='COM4', stack_number=1, UR5_object=UR5)
+    arm.main()
 
-    wpose = geometry_msgs.msg.Pose()
-    wpose.orientation.x =-0.9239
-    wpose.orientation.y = 0.3827
-    wpose.orientation.z = 0.0
-    wpose.orientation.w = 0.0
-    wpose.position.x = 0.4
-    wpose.position.y = -0.2	
-    wpose.position.z = 0.5
-
-
-    waypoints_1 = []
-    waypoints_1.append(copy.deepcopy(wpose))
-
-    cartesian_plan, fraction = UR5.plan_cartesian_path(waypoints=waypoints_1)
+    # wpose = geometry_msgs.msg.Pose()
+    # wpose.orientation.x = 0.0
+    # wpose.orientation.y = 0.7071
+    # wpose.orientation.z = 0.0
+    # wpose.orientation.w = 0.7071
+    # wpose.position.x = 0.3
+    # wpose.position.y = -0.075
+    # wpose.position.z = 0.5
 
 
-    #test_list = [0.75, 0.075, 0.25, 0, 0, 0]
-    #print(test_get_quaternion_from_euler(0, 0, 0))
+    # waypoints_1 = []
+    # waypoints_1.append(copy.deepcopy(wpose))
 
+    # cartesian_plan, fraction = UR5.plan_cartesian_path(waypoints=waypoints_1)
+    # UR5.execute_plan(cartesian_plan)
 
-    print('test')
-    raw_input()
-    UR5.execute_plan(cartesian_plan)
-    print('test2')
-    raw_input()
-    #arm = Arm(COM_port='COM4', stack_number=1, UR5_object=UR5)
-    #arm.main()
+    # test_list = [200, 300, 500] + test_get_quaternion_from_euler(0, 0, 0)
+    # print(test_list)
+    # print(test_list[0] / 1000)
+    #
+
+    # print('test')
+    # raw_input()
+    # UR5.execute_plan(cartesian_plan)
+    # print('test2')
+    # raw_input()
+
 
 				
